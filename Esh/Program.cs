b98a9ac6391e3579a66852a0d724dcc2ebcf94a2
anil parmar <anilparmar76983@gmail.com>
2021-05-07 19:54:47 +0530

    final done

--- Esh/Program.cs
+++ Esh/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using Azure.Identity;
 
 namespace Esh
 {
@@ -18,15 +17,7 @@ namespace Esh
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-/*.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})*/
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
